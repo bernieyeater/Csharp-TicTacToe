83d99f778bcc36d0fc8702f9487ecd1a37717c08
bernieyeater <50559129+bernieyeater@users.noreply.github.com>
2019-07-24 19:27:10 -0400

    Add files via upload
    Tic Tac Toe Game using C# and forms, and the graphics drawing tool.  Thought the game would be a good test of my new C# skills.  The game switches between X and O and requires two players.  The program using the mouse selects the square and draws an X or O.  It also figures out when X or O has won.

---
+++ TicTacToe 004/TicTacToe 004/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace TicTacToe_004
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 414);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(192, 414);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Draw 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(309, 414);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Draw 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(75, 457);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 37);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Draw 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Location = new System.Drawing.Point(111, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(75, 72);
+            this.panel1.TabIndex = 5;
+            this.panel1.Click += new System.EventHandler(this.button1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Location = new System.Drawing.Point(215, 25);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(78, 72);
+            this.panel2.TabIndex = 6;
+            this.panel2.Click += new System.EventHandler(this.button2_Click);
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(192, 457);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 37);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Draw 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(309, 457);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 37);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Draw 6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(75, 500);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 37);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Draw 7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(192, 500);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(111, 37);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Draw 8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(309, 500);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(111, 37);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "Draw 9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(192, 323);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(111, 37);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "New Game";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Control;
+            this.panel3.Location = new System.Drawing.Point(309, 25);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(88, 72);
+            this.panel3.TabIndex = 7;
+            this.panel3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.Control;
+            this.panel4.Location = new System.Drawing.Point(111, 116);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(75, 72);
+            this.panel4.TabIndex = 8;
+            this.panel4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.Control;
+            this.panel5.Location = new System.Drawing.Point(215, 116);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(78, 72);
+            this.panel5.TabIndex = 8;
+            this.panel5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.Control;
+            this.panel6.Location = new System.Drawing.Point(309, 116);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(88, 72);
+            this.panel6.TabIndex = 9;
+            this.panel6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.SystemColors.Control;
+            this.panel7.Location = new System.Drawing.Point(111, 207);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(75, 72);
+            this.panel7.TabIndex = 13;
+            this.panel7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.SystemColors.Control;
+            this.panel8.Location = new System.Drawing.Point(215, 207);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(78, 72);
+            this.panel8.TabIndex = 14;
+            this.panel8.Click += new System.EventHandler(this.button8_Click);
+            this.panel8.Paint += new System.Windows.Forms.PaintEventHandler(this.panel8_Paint);
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.SystemColors.Control;
+            this.panel9.Location = new System.Drawing.Point(309, 208);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(88, 72);
+            this.panel9.TabIndex = 15;
+            this.panel9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(203, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "label1";
+            //this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 661);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TicTacToe 004/TicTacToe 004/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe_004
+{
+    public partial class Form1 : Form
+    {
+        //int SquareNo = 1; //For deciding what Square to put the "X" or "O"
+        int UserTurn = 1;
+        static public int TheWinner = 0;
+
+        //Set the postions for the nine turns
+        int[] Horz = new int[10]; //Holds the vertical position for drawing the matrix.
+        int[] Vert = new int[10]; //Holds the horizonal postiion for drawing the matrix.
+        static public int[] Player = new int[10]; //Keeps track of who is on the square
+
+            public Form1()
+        {
+            InitializeComponent();
+            this.Width = 500;
+            this.Height = 450;
+            label1.Text = " ";
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g;
+            g = e.Graphics;
+            Pen myPen = new Pen(Color.Black);
+            myPen.Width = 5;
+            g.DrawLine(myPen, 100, 100, 400, 100);
+            g.DrawLine(myPen, 100, 200, 400, 200);
+            g.DrawLine(myPen, 200, 25, 200, 300);
+            g.DrawLine(myPen, 300, 25, 300, 300);
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (Horz[i] > 0)
+                {
+                    //Position the X or O
+                    int x1 = Horz[i];
+                    int y1 = Vert[i];
+                    int x2 = Horz[i] + 50;
+                    int y2 = Vert[i] + 50;
+                    
+                    if (Player[i] == 1)
+                    {
+                        //Draw an "X" at the right square
+                        g.DrawLine(myPen, x1, y1, x2, y2);
+                        g.DrawLine(myPen, x2, y1, x1, y2);
+                    }
+                    if (Player[i] == 2)
+                    {
+                        //Draw an "O" at the right square
+                        Point point1 = new Point(x1, y1);
+                        Point point2 = new Point(x2, y1);
+                        Point point3 = new Point(x2, y2);
+                        Point point4 = new Point(x1, y2);
+                        Point point5 = new Point(x1, y1);
+                        Point[] curvePoints = { point1, point2, point3, point4, point5 };
+                        g.DrawCurve(myPen, curvePoints);
+                    }
+                }
+            }
+            //Win Logic for X
+            if ((Player[1] == 1) && (Player[2] == 1) && (Player[3] == 1) && (TheWinner == 0)) { TheWinner = 1; }
+            if ((Player[4] == 1) && (Player[5] == 1) && (Player[6] == 1) && (TheWinner == 0)) { TheWinner = 1; }
+            if ((Player[7] == 1) && (Player[8] == 1) && (Player[9] == 1) && (TheWinner == 0)) { TheWinner = 1; }
+            if ((Player[1] == 1) && (Player[4] == 1) && (Player[7] == 1) && (TheWinner == 0)) { TheWinner = 1; }
+            if ((Player[2] == 1) && (Player[5] == 1) && (Player[8] == 1) && (TheWinner == 0)) { TheWinner = 1; }
+            if ((Player[3] == 1) && (Player[6] == 1) && (Player[9] == 1) && (TheWinner == 0)) { TheWinner = 1; }
+            if ((Player[1] == 1) && (Player[5] == 1) && (Player[9] == 1) && (TheWinner == 0)) { TheWinner = 1; }
+            if ((Player[3] == 1) && (Player[5] == 1) && (Player[7] == 1) && (TheWinner == 0)) { TheWinner = 1; }
+            //Win Logic for O
+            if ((Player[1] == 2) && (Player[2] == 2) && (Player[3] == 2) && (TheWinner == 0)) { TheWinner = 2; }
+            if ((Player[4] == 2) && (Player[5] == 2) && (Player[6] == 2) && (TheWinner == 0)) { TheWinner = 2; }
+            if ((Player[7] == 2) && (Player[8] == 2) && (Player[9] == 2) && (TheWinner == 0)) { TheWinner = 2; }
+            if ((Player[1] == 2) && (Player[4] == 2) && (Player[7] == 2) && (TheWinner == 0)) { TheWinner = 2; }
+            if ((Player[2] == 2) && (Player[5] == 2) && (Player[8] == 2) && (TheWinner == 0)) { TheWinner = 2; }
+            if ((Player[3] == 2) && (Player[6] == 2) && (Player[9] == 2) && (TheWinner == 0)) { TheWinner = 2; }
+            if ((Player[1] == 2) && (Player[5] == 2) && (Player[9] == 2) && (TheWinner == 0)) { TheWinner = 2; }
+            if ((Player[3] == 2) && (Player[5] == 2) && (Player[7] == 2) && (TheWinner == 0)) { TheWinner = 2; }
+
+            if (TheWinner == 1) {label1.Text = "X Wins Game";}
+            if (TheWinner == 2) {label1.Text = "O Wins Game";
+            }
+        }
+        
+        //These "buttons" are hidden by shorting the screen, but they are called from the panels.
+        //After I got the buttons working, I substatuted a call from the panel.
+        //By modifing the button code in Designer.cs
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            TakeTheSquare(1, 130, 30);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+            TakeTheSquare(2, 230, 30);
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel3.Visible = false;
+            TakeTheSquare(3, 330, 30);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panel4.Visible = false;
+            TakeTheSquare(4, 130, 130);
+        }
+       
+        private void button5_Click(object sender, EventArgs e)
+        {
+            panel5.Visible = false;
+            TakeTheSquare(5, 230, 130);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            panel6.Visible = false;
+            TakeTheSquare(6, 330, 130);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            panel7.Visible = false;
+            TakeTheSquare(7, 130, 230);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            panel8.Visible = false;
+            TakeTheSquare(8, 230, 230);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            panel9.Visible = false;
+            TakeTheSquare(9, 330, 230);
+            }
+
+        private void TakeTheSquare(int square, int h, int v)
+        {
+            UserTurn = UserTurn + 1;
+            if (Player[square] == 0)
+            {
+                if ((UserTurn % 2) != 0) { Player[square] = 1; } else { Player[square] = 2; }
+            }
+            Horz[square] = h;
+            Vert[square] = v;
+            this.Refresh();
+        }
+
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //New Game Button!
+            Array.Clear(Horz, 0, 10);  //Reset the Horzonal drawing record.
+            Array.Clear(Vert, 0, 10);  //Reset the Vertical drawing record
+            Array.Clear(Player, 0, 10); //Reset who is on what square.
+            panel1.Visible = true;
+            panel2.Visible = true;
+            panel3.Visible = true;
+            panel4.Visible = true;
+            panel5.Visible = true;
+            panel6.Visible = true;
+            panel7.Visible = true;
+            panel8.Visible = true;
+            panel9.Visible = true;
+            TheWinner = 0;
+            label1.Text = " ";
+            this.Refresh();
+        }
+        private void panel8_Paint(object sender, PaintEventArgs e)
+        { }
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        { }
+        private void panel1_Click(object sender, PaintEventArgs e)
+        { }
+        private void panel2_Click(object sender, PaintEventArgs e)
+        { }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        { }
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+
+    }
+}
